1e84e74d0cd62f07f1da4987572524a05ff5ef6b
William-gingerich <william.gingerich99@gmail.com>
2018-12-11 17:30:27 -0500

    Added first old project

---
+++ Enter_the_metrx/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace enterthematrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            StreamWriter sw = new StreamWriter("output.txt");
+
+            //the magic sauce happends here
+            for (int i = 1; i < lines.Length; i++)
+            {
+                char[] linedata = (lines[i]).ToCharArray();
+                int n = Convert.ToInt16(Math.Sqrt(linedata.Length));
+                char[,] matrix = new char[n, n];
+                int dataindex = 0;
+                int matrixsum = 0;
+
+                //populates matrix with values
+                for (int x = 0; x < n; x++)
+                {
+                    for (int y = 0; y < n; y++)
+                    {
+                        // if statment to stop out of range(should be unnecessary but better safe then sorry)
+                        if (dataindex > linedata.Length)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            matrix[x, y] = linedata[dataindex];
+                        }
+                        dataindex++;
+                    }
+                }
+                //steps through the matrix and does vodo black magic
+                for (int pos1 = 0; pos1 < n; pos1++)
+                {
+                    matrixsum += Convert.ToInt16(matrix[pos1, pos1]);
+                }
+                int x1 = n - 1;
+                int y1 = 0;
+                while (true)
+                {
+                    if (x1 >= 0)
+                    {
+                        matrixsum += Convert.ToInt16(matrix[y1, x1]);
+                        x1--;
+                        y1++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+
+
+                }
+                sw.WriteLine("The sum of the diagonals of matrix {0} is {1}.", (i - 1), matrixsum);
+                Console.WriteLine(matrixsum);
+                Console.ReadKey();
+
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ Enter_the_metrx/README.txt
@@ -0,0 +1,35 @@
+﻿███████╗███╗   ██╗████████╗███████╗██████╗     ████████╗██╗  ██╗███████╗    ███╗   ███╗ █████╗ ████████╗██████╗ ██╗██╗  ██╗
+██╔════╝████╗  ██║╚══██╔══╝██╔════╝██╔══██╗    ╚══██╔══╝██║  ██║██╔════╝    ████╗ ████║██╔══██╗╚══██╔══╝██╔══██╗██║╚██╗██╔╝
+█████╗  ██╔██╗ ██║   ██║   █████╗  ██████╔╝       ██║   ███████║█████╗      ██╔████╔██║███████║   ██║   ██████╔╝██║ ╚███╔╝ 
+██╔══╝  ██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗       ██║   ██╔══██║██╔══╝      ██║╚██╔╝██║██╔══██║   ██║   ██╔══██╗██║ ██╔██╗ 
+███████╗██║ ╚████║   ██║   ███████╗██║  ██║       ██║   ██║  ██║███████╗    ██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║██║██╔╝ ██╗
+╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝       ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝
+  
+DESCRIPTION
+---------------------------------------------------------------------------------------------------------------------------
+In order to crack the Matrix code and return to the one true reality, you must write a program to calculate the sum of the
+diagonals of a series of square character matrices.
+
+
+PERMITTED NAMESPACES
+---------------------------------------------------------------------------------------------------------------------------
+System
+System.IO
+
+
+INPUT SPECIFICATION
+---------------------------------------------------------------------------------------------------------------------------
+The file "input.txt" contains the data for several character matrices.  The first line indicates the number of matrices
+in the file.  The data from each matrix is on a single line, such that each character from the line will fill an NxN matrix 
+starting at the upper-left corner.
+
+For example, the data k&%8sk)!0 will fill a 3x3 matrix as...
+
+k&%
+8sk
+)!0                                                                                                                        
+
+
+OUTPUT SPECIFICATION
+----------------------------------------------------------------------------------------------------------------------------
+Your program should match "sample-output.txt" exactly.  Note that characters are summed according to their ASCII codes.
